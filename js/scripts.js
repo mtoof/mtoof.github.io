104b3e5ad2e0a4270cacec5057fb29f5796800f2
Mojtaba toof <mojtaba.toof@gmail.com>
2024-10-06 09:33:29 +0300

    fixed overlapping section active for scrolling and align page-section padding to 10rem

--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,8 @@
               if (entry.target.id === "hero")
                 history.replaceState(null, null, `/`);
             else
-            history.replaceState(null, null, `#${entry.target.id}`);
-        navLinks.forEach(link => link.classList.remove('active'));
+                history.replaceState(null, null, `#${entry.target.id}`);
+            // navLinks.forEach(link => link.classList.remove('active'));
             if (entry.target.id != "hero")
                 navLink.classList.add('active');
         }
@@ -56,16 +56,16 @@ window.addEventListener('DOMContentLoaded', event => {
     navbarShrink();
 
     // Shrink the navbar when page is scrolled
-    // document.addEventListener('scroll', navbarShrink);
+    document.addEventListener('scroll', navbarShrink);
 
     // // Activate Bootstrap scrollspy on the main nav element
-    // const mainNav = document.body.querySelector('#mainNav');
-    // if (mainNav) {
-    //     new bootstrap.ScrollSpy(document.body, {
-    //         target: '#mainNav',
-    //         rootMargin: '0px 0px -40%',
-    //     });
-    // };
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
